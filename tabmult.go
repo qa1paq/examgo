1ffae3c785329eeb5b03f6107b0a613c1ed80128
Your Name <you@example.com>
2022-06-09 17:44:40 +0600

    21

---
+++ tabmult.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println()
+		return
+	}
+	a := 0
+	for _, r := range args[0] {
+		if r >= '0' && r <= '9' {
+			a = a*10 + int(r-'0')
+		} else {
+
+			return
+		}
+	}
+	for i := 1; i <= 9; i++ {
+		fmt.Printf("%d x %d", i, a)
+		fmt.Println()
+	}
+
+}
