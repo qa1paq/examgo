87f76f963becf01cb5d7c5765284e114c628097b
qa1paq <you@example.com>
2022-06-11 00:10:19 +0600

    25

--- findprevprime.go
+++ findprevprime.go
@@ -6,11 +6,14 @@ import (
 
 func main() {
 	fmt.Println(FindPrevPrime(11))
-	fmt.Println(FindPrevPrime(4))
+	fmt.Println(FindPrevPrime(1))
 }
 
 func FindPrevPrime(nb int) int {
 	y := false
+	if nb == 2 {
+		return 2
+	}
 	for nb >= 2 {
 		// fmt.Println(nb)
 		for i := 2; i < nb; i++ {
