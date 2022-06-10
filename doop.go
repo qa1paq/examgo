fe674fd97983bfda2cbdb9573dcb88c15075ee80
qalpaq <you@example.com>
2022-06-10 21:16:28 +0600

    24

---
+++ doop.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 3 {
+		fmt.Println()
+		return
+	}
+	d := 1
+	c := 1
+	a := 0
+	b := 0
+	for _, r := range args[0] {
+		if r >= '0' && r <= '9' {
+			a = a*10 + int(r-'0')
+		} else if args[0][0] == '-' {
+			c = -1
+		} else {
+			fmt.Println("0")
+			return
+		}
+	}
+	for _, r := range args[2] {
+		if r >= '0' && r <= '9' {
+			b = b*10 + int(r-'0')
+		} else if args[2][0] == '-' {
+			d = -1
+		} else {
+			fmt.Println("0")
+			return
+		}
+	}
+	b = b * d
+	a = a * c
+	if args[1][0] == '+' {
+		fmt.Println(a + b)
+	} else if args[1][0] == '-' {
+		fmt.Println(a - b)
+	} else if args[1][0] == '*' {
+		fmt.Println(a * b)
+	} else if args[1][0] == '/' {
+		if b == 0 {
+			fmt.Println("No division by 0")
+		} else {
+			fmt.Println(a / b)
+		}
+	} else if args[1][0] == '%' {
+		if b == 0 {
+			fmt.Println("No modulo by 0")
+		} else {
+			fmt.Println(a % b)
+		}
+	} else {
+		fmt.Println("0")
+		return
+	}
+}
