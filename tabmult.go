17ba6ba4386bbd5f01e8500b51114cb3ad6f4583
qa1paq <you@example.com>
2022-06-11 00:34:49 +0600

    tab

--- tabmult.go
+++ tabmult.go
@@ -16,13 +16,11 @@ func main() {
 		if r >= '0' && r <= '9' {
 			a = a*10 + int(r-'0')
 		} else {
-
 			return
 		}
 	}
 	for i := 1; i <= 9; i++ {
-		fmt.Printf("%d x %d", i, a)
+		fmt.Printf("%d x %d = %d", i, a, i*a)
 		fmt.Println()
 	}
-
 }
